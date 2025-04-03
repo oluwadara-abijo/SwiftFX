616c7c77a13b92693d1a5a44268c962ab49f9408
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2025-04-03 09:44:22 +0100

    feat: Set up app theme

--- app/src/main/java/com/dara/swiftfx/ui/theme/Type.kt
+++ app/src/main/java/com/dara/swiftfx/ui/theme/Type.kt
@@ -2,33 +2,48 @@ package com.dara.swiftfx.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.dara.swiftfx.R
 
-// Set of Material typography styles to start with
+val brFirmaFamily = FontFamily(
+    Font(R.font.br_firma_bold, FontWeight.Bold),
+    Font(R.font.br_firma_light, FontWeight.Light),
+    Font(R.font.br_firma_medium, FontWeight.Medium),
+    Font(R.font.br_firma_regular, FontWeight.Normal),
+    Font(R.font.br_firma_semi_bold, FontWeight.SemiBold),
+    Font(R.font.br_firma_thin, FontWeight.Thin),
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
+    headlineLarge = TextStyle(
+        fontFamily = brFirmaFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+        lineHeight = 32.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = brFirmaFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+    bodyLarge = TextStyle(
+        fontFamily = brFirmaFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = brFirmaFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
-    )
-    */
+    ),
 )
